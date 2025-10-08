406cae61a23242185364db2422de55dd26150c87
oaemdl <oaemdl@gmail.com>
2025-10-08 10:16:16 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express"
+import cors from "cors";
 import adminRouter from "./v1/routes/admin.routes.js"
 
 const app = express()
 
+app.use(cors());
 app.use( express.json() )
 app.use("/api/v1/admin", adminRouter)
 
